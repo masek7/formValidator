a81d862799289fa6a3f6b4ceb606e7cbf4ef966f
Guilherme Oliveira <74804474+masek7@users.noreply.github.com>
2021-11-05 12:03:27 -0300

    Add files via upload

---
+++ form.js
@@ -0,0 +1,80 @@
+
+
+    var nome = document.getElementById('nameInput');
+    var sobrenome= document.getElementById('sobrenomeInput');
+    var email = document.getElementById('emailInput');
+    var senha = document.getElementById('senhaInput');
+    var telefone = document.getElementById('telefoneInput');
+    var cep = document.getElementById('cepInput');
+    var divSenha= document.getElementById('senha');
+
+
+    var tudoCerto;
+
+    function verificar() {
+        tudoCerto=true;
+
+    if(nome.value ==""){
+        nome.style.borderColor ="red";
+        tudoCerto = false;
+    }
+
+    if(sobrenome.value ==""){
+        sobrenome.style.borderColor ="red";
+        tudoCerto = false;
+    } 
+
+    if(email.value =="" || email.value.indexOf('@') ==-1){
+        email.style.borderColor ="red";
+        tudoCerto = false;
+    }
+
+    if(senha.value =="" || senha.value.length < 8){
+        senha.style.borderColor ="red";
+        tudoCerto = false;
+    }
+
+    if(telefone.value ==""){
+        telefone.style.borderColor ="red";
+        tudoCerto = false;
+    }
+
+    if(cep.value.length <=0){
+        cep.style.borderColor ="red";
+        tudoCerto = false;
+    }
+
+    if(tudoCerto ==true){
+        nome.value ="";
+        sobrenome.value= "";
+        email.value= "";
+        senha.value= "";
+        telefone="";
+        cep.value="";
+        nome.style.borderColor= "rgb(238, 238, 238)";
+        sobrenome.style.borderColor= "rgb(238, 238, 238)";
+        email.style.borderColor= "rgb(238, 238, 238)";
+        senha.style.borderColor= "rgb(238, 238, 238)";
+        telefone.style.borderColor= "rgb(238, 238, 238)";
+        cep.style.borderColor= "rgb(238, 238, 238)";
+    } 
+    }
+        
+    
+
+    
+    
+
+/*criar uma variavel verdadeira ou falsa e caso ela seja verdadeira limpar todos os campos, se não for, não limpar.borderColor*/
+
+/* algo como
+ if(tudoCerto == true){
+             resetar inputs
+        
+    
+    }
+    }*/
+    
+
+
+
